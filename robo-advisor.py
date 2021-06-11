0bcbbd8aa85c1a826b4a5250c12b1b641816b648
SpencerBranch <65453198+SpencerBranch@users.noreply.github.com>
2021-06-10 20:50:41 -0400

    First data pull

--- robo-advisor.py
+++ robo-advisor.py
@@ -1,1 +1,14 @@
-print("make a web request for stock market data")
+import os
+import requests
+from dotenv import load_dotenv
+
+# "https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=BTC&apikey="
+symbol = "MSFT"
+Alpha_API_Key = os.getenv("Alpha_API_Key")
+func = "TIME_SERIES_DAILY_ADJUSTED"
+
+base_url = f"https://www.alphavantage.co/query?function={func}&symbol={symbol}&apikey={Alpha_API_Key}"
+x = requests.get(base_url)
+print(x.text)
+
+# Make a request
